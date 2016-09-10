24efa2b19ccecbd2dfafcb77514ab2969a260a54
zheka <zheka0783@yandex.ru>
2016-09-10 21:05:19 +0300

    change1

--- test_shell/test2.sh
+++ test_shell/test2.sh
@@ -1,2 +1,15 @@
 # watchfor: cледит за входом в систему
 
+old=/tmp/watchfor1.$$
+new=/tmp/watchfor2.$$
+
+>$old
+
+while :
+do
+	who > $new
+	diff $old $new
+	mv $new $old
+	sleep 1
+done | awk '/>/ { print $1 }
+			'/</ { print $1 }'
